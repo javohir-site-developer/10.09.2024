1afc7ac177c54ea7e0c42673f1fce8ca753db25c
javohir-site-developer <javohirjasurovich999@gmail.com>
2024-09-10 22:49:56 +0500

    ¨javohirniki¨

---
+++ main.js
@@ -0,0 +1,40 @@
+//birinchi vazifa
+
+numberSecond = 3;
+numberFirst = 2;
+
+console.log(numberSecond + numberFirst);
+
+// second homework
+
+number3 = 4;
+number4 = 2;
+
+console.log(number3 - number4);
+
+//third homework
+
+number5 = 4;
+number6 = 2;
+
+console.log(number5 - number6);
+
+//four homework
+
+number7 = 4;
+number8 = 2;
+
+console.log(number7 * number8);
+
+//five homework
+
+number9 = 4;
+number10 = 2;
+
+console.log(number9 / number10);
+
+//six homework
+
+number1 = 3;
+number0 = console.log(number1);
+console.log(number0);
